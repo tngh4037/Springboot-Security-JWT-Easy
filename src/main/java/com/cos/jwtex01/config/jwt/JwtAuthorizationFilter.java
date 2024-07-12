87eab192ecb80ff1219cc2e274e98065bddcc127
tngh4037 <tngh4037@gmail.com>
2024-07-13 06:17:32 +0900

    27강 - jwt토큰 서버 구축 완료

--- src/main/java/com/cos/jwtex01/config/jwt/JwtAuthorizationFilter.java
+++ src/main/java/com/cos/jwtex01/config/jwt/JwtAuthorizationFilter.java
@@ -19,8 +19,11 @@ import com.cos.jwtex01.config.auth.PrincipalDetails;
 import com.cos.jwtex01.model.User;
 import com.cos.jwtex01.repository.UserRepository;
 
-// 인가
-public class JwtAuthorizationFilter extends BasicAuthenticationFilter{
+// [인가]
+// 시큐리티가 filter 를 가지고 있는데, 그 필터중에 BasicAuthenticationFilter 라는 것이 있다.
+// 인증이나 권한이 필요한 특정 주소를 요청했을 때, 위 필터를 무조건 타게 되어있다.
+// 만약에 인증이나 권한이 필요한 주소가 아니라면 이 필터를 타지 않는다.
+public class JwtAuthorizationFilter extends BasicAuthenticationFilter {
 	
 	private UserRepository userRepository;
 	
@@ -32,25 +35,30 @@ public class JwtAuthorizationFilter extends BasicAuthenticationFilter{
 	@Override
 	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain)
 			throws IOException, ServletException {
+		System.out.println("인증이나 권한이 필요한 주소가 요청 됨.");
+
 		String header = request.getHeader(JwtProperties.HEADER_STRING);
-		if(header == null || !header.startsWith(JwtProperties.TOKEN_PREFIX)) {
+		if (header == null || !header.startsWith(JwtProperties.TOKEN_PREFIX)) {
 			chain.doFilter(request, response);
-                        return;
+			return;
 		}
-		System.out.println("header : "+header);
+		System.out.println("header : " + header);
+
+		// ================================================
+		// ==== JWT 토큰을 검증해서 정상적인 사용자인지 확인 ====
+		// ================================================
 		String token = request.getHeader(JwtProperties.HEADER_STRING)
 				.replace(JwtProperties.TOKEN_PREFIX, "");
 		
-		// 토큰 검증 (이게 인증이기 때문에 AuthenticationManager도 필요 없음)
-		// 내가 SecurityContext에 집적접근해서 세션을 만들때 자동으로 UserDetailsService에 있는 loadByUsername이 호출됨.
-		String username = JWT.require(Algorithm.HMAC512(JwtProperties.SECRET)).build().verify(token)
-				.getClaim("username").asString();
+		// JWT 토큰 검증 (이게 인증이기 때문에 AuthenticationManager도 필요 없음)
+		String username = JWT.require(Algorithm.HMAC512(JwtProperties.SECRET)).build().verify(token) // 서명
+				.getClaim("username").asString(); // 서명이 정상적으로 되면 username 을 가져온다.
 		
-		if(username != null) {	
+		if (username != null) {
 			User user = userRepository.findByUsername(username);
 			
 			// 인증은 토큰 검증시 끝. 인증을 하기 위해서가 아닌 스프링 시큐리티가 수행해주는 권한 처리를 위해 
-			// 아래와 같이 토큰을 만들어서 Authentication 객체를 강제로 만들고 그걸 세션에 저장! ( 17:48 세션에 담지 않으면 SecurityConfig 에 설정한 권한 관리가 안됨. 스프링 시큐리티는 세션을 기반으로 권한관리를 한다. 만약에 이런 권한관리를 안할거면 굳이 세션에 담을 필요는 없다. 세션에 담는 이유는 권한 관리를 위해서이다. (굳이 JWT 토큰을 사용하면서 세션을 만들 이유가 없음. 근데 단지 권한 처리때문에 session 에 넣어준다.) )
+			// 아래와 같이 토큰을 만들어서 Authentication 객체를 강제로 만들고 그걸 세션에 저장! ( 17:48 세션에 담지 않으면 SecurityConfig 에 설정한 권한 관리가 안됨. 스프링 시큐리티는 세션을 기반으로 권한관리를 한다. 만약에 이런 권한관리를 안할거면 굳이 세션에 담을 필요는 없다. 세션에 담는 이유는 권한 관리를 위해서이다. (굳이 JWT 토큰을 사용하면서 세션을 만들 이유가 없음. 근데 단지 권한 처리때문에 세션에 넣어준다.) )
 			PrincipalDetails principalDetails = new PrincipalDetails(user);
 			Authentication authentication =
 					new UsernamePasswordAuthenticationToken(
@@ -58,11 +66,14 @@ public class JwtAuthorizationFilter extends BasicAuthenticationFilter{
 							null, // 패스워드는 모르니까 null 처리, 어차피 지금 인증하는게 아니니까!!
 							principalDetails.getAuthorities());
 			
-			// 강제로 시큐리티의 세션에 접근하여 값 저장 ( 의문 : 근데 이게 세션인가 ? 메모리 라고 생각하는게 더 맞아보임. | 참고) https://www.inflearn.com/questions/714149/jwt-token-%EA%B5%AC%ED%98%84%EC%97%90%EC%84%9C-session-%EC%9D%84-%EC%82%AC%EC%9A%A9%ED%95%9C%EB%8B%A4 )
+			// 강제로 시큐리티의 세션에 접근하여 Authentication 객체를 저장
 			SecurityContextHolder.getContext().setAuthentication(authentication);
 		}
-	
+
 		chain.doFilter(request, response);
 	}
 	
 }
+
+// 참고) https://www.inflearn.com/questions/714149/jwt-token-%EA%B5%AC%ED%98%84%EC%97%90%EC%84%9C-session-%EC%9D%84-%EC%82%AC%EC%9A%A9%ED%95%9C%EB%8B%A4
+// 참고) https://www.inflearn.com/questions/740712/session-%EA%B3%BC-securitycontext%EC%97%90-%EA%B4%80%EB%A0%A8%EB%90%9C-%EC%A7%88%EB%AC%B8%EC%9E%85%EB%8B%88%EB%8B%A4
--- src/main/java/com/cos/jwtex01/controller/RestApiController.java
+++ src/main/java/com/cos/jwtex01/controller/RestApiController.java
@@ -36,13 +36,8 @@ public class RestApiController {
 		return "<h1>token</h1>";
 	}
 
-
 	// ==============================================================================
 
-	
-	// Tip : JWT를 사용하면 UserDetailsService를 호출하지 않기 때문에 @AuthenticationPrincipal 사용 불가능.
-	// 왜냐하면 @AuthenticationPrincipal은 UserDetailsService에서 리턴될 때 만들어지기 때문이다.
-	
 	// 유저 혹은 매니저 혹은 어드민이 접근 가능
 	@GetMapping("user")
 	public String user(Authentication authentication) {
@@ -76,14 +71,3 @@ public class RestApiController {
 	}
 	
 }
-
-
-
-
-
-
-
-
-
-
-
